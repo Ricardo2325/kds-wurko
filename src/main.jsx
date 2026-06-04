a7aeaa3920f2cc2233a16a26f8781ccb7be5f7a8
Ricardo2325 <ricardorodriguezdelgado6@gmail.com>
2026-06-04 16:25:08 +0100

    feat: add Vite + React setup for Vercel deployment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import KDS from '../KDS.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KDS />
+  </React.StrictMode>
+);
